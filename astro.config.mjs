7ce4aa8a2fd684ade20c673507c1798205f19055
PremAcoustic <sinkicsgo@gmail.com>
2024-05-29 17:42:06 +0100

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,26 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import netlify from '@astrojs/netlify';
+import solid from '@astrojs/solid-js';
+import { resolve } from 'path';
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://stellular-licorice-c2bfcd.netlify.app/",
-  integrations: [tailwind(), mdx(), icon(), sitemap()],
+  integrations: [tailwind(), mdx(), icon(), sitemap(), solid(), netlify(), svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        '@lib': resolve('./src/lib'),
+        '@utils': resolve('./src/utils'),
+        '@components': resolve('./src/components'),
+        '@layouts': resolve('./src/layouts'),
+        '@assets': resolve('./src/assets'),
+        '@stores': resolve('./src/stores'),
+        '@pages': resolve('./src/pages')
+      }
+    }
+  }
 });
